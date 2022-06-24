1a4341b05b25d512fb95123d3de519fd5819c25f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-24 17:52:40 +0530

    Update User.jsx

--- front/src/Components/User.jsx
+++ front/src/Components/User.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import "./Css/User.css";
 
 export default function User() {
   return (
     <div>
-      <input type="text" name="name" id="" placeholder="Enter Your Name" />
-      <input type="text" name="age" id="" placeholder="Enter your Age" />
-      <input type="text" name="sex" id="" placeholder="Enter your Sex" />
-      <input
-        type="text"
-        name="address"
-        id=""
-        placeholder="Enter your Address"
-      />
+      <div>
+        <input type="text" name="name" id="" placeholder="Enter Your Name" />
+        <input type="text" name="age" id="" placeholder="Enter your Age" />
+        <input type="text" name="sex" id="" placeholder="Enter your Sex" />
+        <input
+          type="text"
+          name="address"
+          id=""
+          placeholder="Enter your Address"
+        />
+      </div>
     </div>
   );
 }
