c31cca444ecb1eed7040e2c6daed552eb6e72a36
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-24 21:13:49 +0530

    Update Particulars.jsx

--- front/src/Components/UploadFile/Particulars.jsx
+++ front/src/Components/UploadFile/Particulars.jsx
@@ -1,13 +1,76 @@
 import React, { useState } from "react";
 
 export default function Particulars() {
+  const [showData, setData] = useState({
+    name: "",
+    age: "",
+    sex: "",
+    address: "",
+    travel: "",
+    From: "",
+    To: "",
+  });
+
+  const DataInp = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setData({ ...showData, [name]: value });
+    console.log(showData);
+  };
+
+  const PostData = async () => {
+    console.log(showData);
+
+    setData({
+      ...showData,
+      name: "",
+      age: "",
+      sex: "",
+      address: "",
+      travel: "",
+      From: "",
+      To: "",
+    });
+  };
   return (
     <div>
       <p className="Particulars">Particulars</p>
-      <input type="text" className="PartInp" placeholder="Enter Your Name" />
-      <input type="text" className="PartInp" placeholder="Enter Your Age" />
-      <input type="text" className="PartInp" placeholder="Enter Your Sex" />
-      <input type="text" className="PartInp" placeholder="Enter Your Address" />
+      <input
+        type="text"
+        name="name"
+        className="PartInp"
+        placeholder="Enter Your Name"
+        value={showData.name}
+        onChange={DataInp}
+      />
+      <input
+        type="text"
+        name="age"
+        className="PartInp"
+        placeholder="Enter Your Age"
+        value={showData.name}
+        onChange={DataInp}
+      />
+      <input
+        type="text"
+        name="sex"
+        className="PartInp"
+        placeholder="Enter Your Sex"
+        value={showData.name}
+        onChange={DataInp}
+      />
+      <input
+        type="text"
+        name="address"
+        className="PartInp"
+        placeholder="Enter Your Address"
+        value={showData.name}
+        onChange={DataInp}
+      />
+      <button className="btn btn-primary btn-block mt-4" onClick={PostData}>
+        Submit
+      </button>
     </div>
   );
 }
