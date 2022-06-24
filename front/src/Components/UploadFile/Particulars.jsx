5435ebe40cfa4eabf760096a43f59deb3c01c40a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-24 21:10:14 +0530

    Update Particulars.jsx

--- front/src/Components/UploadFile/Particulars.jsx
+++ front/src/Components/UploadFile/Particulars.jsx
@@ -3,8 +3,11 @@ import React from "react";
 export default function Particulars() {
   return (
     <div>
-      <p>Particulars</p>
+      <p className="Particulars">Particulars</p>
       <input type="text" className="PartInp" placeholder="Enter Your Name" />
+      <input type="text" className="PartInp" placeholder="Enter Your Age" />
+      <input type="text" className="PartInp" placeholder="Enter Your Sex" />
+      <input type="text" className="PartInp" placeholder="Enter Your Address" />
     </div>
   );
 }
