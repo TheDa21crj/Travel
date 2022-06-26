ff08b046a7d52bb5faadc1e98dc8c227a0d6a5ef
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-26 09:16:17 +0530

    Update Particulars.jsx

--- front/src/Components/UploadFile/Particulars.jsx
+++ front/src/Components/UploadFile/Particulars.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { useJsApiLoader, Autocomplete } from "@react-google-maps/api";
+// import { useJsApiLoader, Autocomplete } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
 
 export default function Particulars() {
   // const google = window.google;
@@ -52,6 +57,44 @@ export default function Particulars() {
       });
     }
   };
+
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+  };
+
+  const handleSelect =
+    ({ description }) =>
+    () => {
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        console.log("📍 Coordinates: ", { lat, lng });
+      });
+    };
 
   return (
     <div>
