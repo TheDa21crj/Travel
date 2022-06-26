33b34966327255ac1d2d36c300f711a6a9e00e1d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-06-26 09:17:41 +0530

    Update Particulars.jsx

--- front/src/Components/UploadFile/Particulars.jsx
+++ front/src/Components/UploadFile/Particulars.jsx
@@ -96,6 +96,20 @@ export default function Particulars() {
       });
     };
 
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
   return (
     <div>
       <p className="Particulars">Particulars</p>
